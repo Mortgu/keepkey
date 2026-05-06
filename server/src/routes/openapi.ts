8c633cad9fe6f9392d2c2655a1788403e510c3cb
Oskar Sammet <oskar.sammet@gmail.com>
2026-05-06 13:35:05 +0200

    Update openapi.ts

--- server/src/routes/openapi.ts
+++ server/src/routes/openapi.ts
@@ -5,27 +5,9 @@
  */
 
 import { Router } from "express";
-import swaggerUi from "swagger-ui-express";
-import { generateOpenAPISpec } from "../lib/openapi.js";
 
 const router = Router();
 
-// Generate the OpenAPI spec
-const openAPISpec = generateOpenAPISpec();
 
-// Serve OpenAPI JSON
-router.get("/openapi.json", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify(openAPISpec, null, 2));
-});
-
-// Serve Swagger UI
-router.use("/docs", swaggerUi.serve);
-router.get(
-  "/docs",
-  swaggerUi.setup(openAPISpec, {
-    customCss: ".swagger-ui .topbar { display: none }",
-  }),
-);
 
 export default router;
