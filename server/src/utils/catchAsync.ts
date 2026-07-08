2231be1098ab8c99a45c2f4c2447c1d42b3682d4
Oskar Sammet <oskar.sammet@gmail.com>
2026-07-08 11:11:44 +0200

    removed dead code

--- server/src/utils/catchAsync.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-const catchAsync = (fn: any) => (request: Request, response: Response, next: NextFunction) =>
-    Promise.resolve(fn(request, response, next)).catch(next);
-
-export default catchAsync;
