e8f86a6351b7413d0d853a8a53dc13afc105c6f8
Oskar Sammet <oskar.sammet@gmail.com>
2026-07-12 15:43:33 +0200

    env-nextjs to env-core

--- server/src/lib/env.ts
+++ server/src/lib/env.ts
@@ -1,5 +1,5 @@
-import {createEnv} from "@t3-oss/env-nextjs";
-import {z} from "zod";
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
 
 import "dotenv/config";
 
@@ -32,7 +32,7 @@ const env = createEnv({
         CORS_ORIGIN: z.string(),
     },
 
-    experimental__runtimeEnv: {},
+    runtimeEnv: process.env,
 });
 
 export default env;
