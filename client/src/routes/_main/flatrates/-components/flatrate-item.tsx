6aaf2993f702e4ed30f9a9a2400be5eda87d37a3
Oskar Sammet <oskar.sammet@gmail.com>
2026-07-14 11:31:22 +0200

    minor html css fixes

--- client/src/routes/_main/flatrates/-components/flatrate-item.tsx
+++ client/src/routes/_main/flatrates/-components/flatrate-item.tsx
@@ -12,15 +12,15 @@ export default function FlatRateItem({ item }: { item: FlatRate }) {
 
   return (
     <>
-      <div className="bg-white border border-(--border) rounded-md overflow-hidden">
-        <div className="flex items-center justify-between px-4 py-3">
-          <div>
+      <div className="bg-(--page-bg) border border-(--border) rounded-md overflow-hidden">
+        <div className="flex flex-wrap items-center justify-between ">
+          <div className="px-4 py-3 gap-4">
             <p className="text-md text-gray-900">{localized(item.translations, locale, "name")}</p>
             <p className="text-sm font-light text-gray-400 mt-0.5">
               {localized(item.translations, locale, "table")}
             </p>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center justify-between w-full px-4 py-1 gap-4 bg-white border-t border-(--border)">
             <div className="flex items-center gap-6 text-sm text-gray-500 mr-2">
               <span>
                 Gesamt:{" "}
@@ -29,10 +29,10 @@ export default function FlatRateItem({ item }: { item: FlatRate }) {
                 </span>
               </span>
             </div>
-            <div className="flex items-center gap-1">
-              <Button variant="ghost" size="sm" icon={<Pen className="size-3.5" />}
+            <div className="flex items-center">
+              <Button variant="link" size="xs" icon={<Pen className="size-3.5" />}
                 iconOnly onClick={() => modal.open(item)} />
-              <Button variant="ghost" size="sm" loading={isDeletingFlatRate}
+              <Button variant="link" size="xs" loading={isDeletingFlatRate}
                 icon={<Trash className="size-3.5" />} iconOnly
                 onClick={() => deleteFlatRate(item.id)} />
             </div>
