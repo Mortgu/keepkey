c41eaf07f42ecdc49dda9ba3f181e660c7764274
Oskar Sammet <oskar.sammet@gmail.com>
2026-07-21 15:02:23 +0200

    save

--- client/src/routes/_main/customers/-components/customer-list.tsx
+++ client/src/routes/_main/customers/-components/customer-list.tsx
@@ -1,82 +1,47 @@
-import { useTranslation } from "react-i18next";
 import CustomerListItem from "./customer-list-item";
 import CustomerModal from "./customer-modal";
 
-import { Button, ListSkeleton, PageWidth, RouteError, SearchBar, Skeleton, SortDropdown } from "@/components";
+import { ListSkeleton, PageWidth, RouteError, Skeleton } from "@/components";
 import { useCustomers, useModal } from "@/hooks";
-import { Plus } from "lucide-react";
-import { Fragment, useState } from "react";
-
-const sortOptions = [
-    { value: "createdAt:desc", label: "Datum – neuestes zuerst" },
-    { value: "createdAt:asc", label: "Datum – ältestes zuerst" },
-    { value: "orders:asc", label: "Orders - asc" },
-    { value: "orders:desc", label: "Orders - Desc" },
-];
+import { Fragment } from "react";
 
 
 export default function CustomerList() {
-    const { t } = useTranslation();
-
     const modal = useModal();
+
     const { customers, isPending, error } = useCustomers();
 
-    const [query, setQuery] = useState("");
-    const [sort, setSort] = useState(sortOptions[0].value);
-
     return (
-        <PageWidth variant="none">
-            {/* Header */}
-            <div className="flex  items-center gap-4 border-b border-(--border) p-4">
-                <SortDropdown
-                    value={sort}
-                    onChange={setSort}
-                    options={sortOptions}
+        <PageWidth variant="full">
+            {isPending && (
+                <ListSkeleton
+                    rows={6}
+                    skeleton={<Skeleton shape="rect" />}
                 />
-                <SearchBar value={query} onChange={setQuery} placeholder="Search Customer, Contact Person, Phone, E-Mail..." />
-                <Button size="sm" icon={
-                    <Plus size={14} />
-                } onClick={() => modal.open()}>{t("button.create")}</Button>
+            )}
+
+            {error && (
+                <RouteError error={error} />
+            )}
+
+            {(customers.length === 0) && (
+                <p className="text-sm text-(--text-secondary) py-8 text-center">
+
+                </p>
+            )}
+
+
+            <div className="grid gap-2">
+                {customers.map((item, index) => (
+                    <Fragment key={item.id}>
+                        <CustomerListItem customer={item} />
+                    </Fragment>
+                ))}
             </div>
 
-            {/* Filter */}
-            <div className="flex  items-center gap-4">
-
-            </div>
-
-            <PageWidth variant="full">
-
-                {isPending && (
-                    <ListSkeleton
-                        rows={6}
-                        skeleton={<Skeleton shape="rect" />}
-                    />
-                )}
-
-                {error && (
-                    <RouteError error={error} />
-                )}
-
-                {(customers.length === 0) && (
-                    <p className="text-sm text-(--text-secondary) py-8 text-center">
-
-                    </p>
-                )}
-
-
-                <div className="grid gap-2">
-                    {customers.map((item, index) => (
-                        <Fragment key={item.id}>
-                            <CustomerListItem customer={item} />
-                        </Fragment>
-                    ))}
-                </div>
-
-                {modal.isOpen && (
-                    <CustomerModal key={modal.key} onClose={modal.close} />
-                )}
-
-            </PageWidth>
+            {modal.isOpen && (
+                <CustomerModal key={modal.key} onClose={modal.close} />
+            )}
         </PageWidth>
     );
 }
--- client/src/routes/_main/customers/-page.tsx
+++ client/src/routes/_main/customers/-page.tsx
@@ -1,9 +1,50 @@
-import { PageWidth } from "@/components";
+import { Button, PageWidth, SearchBar, SortDropdown } from "@/components";
 import CustomerList from "./-components/customer-list";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { useModal } from "@/hooks";
+import { useTranslation } from "react-i18next";
+
+const sortOptions = [
+    { value: "createdAt:desc", label: "Datum – neuestes zuerst" },
+    { value: "createdAt:asc", label: "Datum – ältestes zuerst" },
+    { value: "orders:asc", label: "Orders - asc" },
+    { value: "orders:desc", label: "Orders - Desc" },
+];
 
 export default function CustomerPage({ }) {
+    const { t } = useTranslation();
+
+    const [query, setQuery] = useState("");
+    const [sort, setSort] = useState(sortOptions[0].value);
+
+    const modal = useModal();
+
     return (
-        <PageWidth variant="none" className="flex">
+        <PageWidth variant="none">
+
+            <div className="p-4 border-b border-(--border)">
+                <h1 className="text-lg font-semibold">{t("section.customers")}</h1>
+                <div className="flex items-center gap-1 text-sm font-light text-(--text-secondary)">
+                    <a>dashboard</a>
+                    <p>></p>
+                    <a>customers</a>
+                </div>
+            </div>
+
+            {/* Header */}
+            <div className="flex items-center gap-4 border-b border-(--border) p-4">
+                <SortDropdown
+                    value={sort}
+                    onChange={setSort}
+                    options={sortOptions}
+                />
+                <SearchBar value={query} onChange={setQuery} placeholder="Search Customer, Contact Person, Phone, E-Mail..." />
+                <Button size="sm" icon={
+                    <Plus size={14} />
+                } onClick={() => modal.open()}>{t("button.create")}</Button>
+            </div>
+
             <CustomerList />
         </PageWidth>
     )
